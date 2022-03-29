07bc0fcbe12d517438239c08e9af2149a4a108d7
unaxgomez <unaxhp@hotmail.com>
2022-03-29 15:56:12 +0200

    login

--- front/src/app/user/user.service.ts
+++ front/src/app/user/user.service.ts
@@ -23,7 +23,7 @@ export class UserService {
   }
 
   checkUser(email: string, password: string): Observable<number>{
-    return this.http.get<number>('https://localhost:44377/api/users/login/' + email + '/' + password)
+    return this.http.get<number>('https://localhost:44377/api/Users/login/' + email + '/' + password)
   }
   
   postUser(user: User):Observable<User>{
