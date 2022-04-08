38390ec6e361efadb6cde19a3382f4fb98bd1e80
Peio Murguia <43602810+PMurguia@users.noreply.github.com>
2022-04-08 15:46:38 +0200

    Update calendar.component.ts

--- front/src/app/calendar/calendar.component.ts
+++ front/src/app/calendar/calendar.component.ts
@@ -37,15 +37,4 @@ export class CalendarComponent implements OnInit {
       editable: false,
     }
   }
-  
-    getdataBookings(): void {
-    this.CalendarService.getData().subscribe(data => {
-      this.calendar = data;
-      // console.log(this.calendar);
-      this.calendar.forEach(cal => {
-        this.arrayPeio.push({ title: cal.userName + " \n " + cal.purposeName, start: cal.dateIn, end: cal.dateOut, color: cal.roomColor })
-      });
-      this.events = this.arrayPeio
-    });
-  }
 }
